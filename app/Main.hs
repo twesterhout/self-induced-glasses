8bba1b706045903e29e3af834626fa0c3be0cd4e
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-03-01 15:08:15 +0100

    Implement resummation of couplings for periodic boundaries. This properly takes into account long-range interaction. Currently we're limited by summing over O(100 x 100) supercells, but perhaps this is already not too bad

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
 module Main (main) where
 
-import SelfInducedGlasses (someFunc)
-
+import Control.Monad
+import SelfInducedGlasses
+import System.IO
 
 main :: IO ()
-main = someFunc
+main = do
+  withFile "resummation.dat" WriteMode $ \h ->
+    forM_ (take 2500 $ effectiveInteractionDebug testModel1 (P 2 1)) $ \(r, δj, j) ->
+      when (r `mod` 100 == 0) $
+        hPutStrLn h (show r <> "\t" <> show δj <> "\t" <> show j)
