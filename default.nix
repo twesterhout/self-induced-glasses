873cf388234380f040041adf4515e4dceb0ef487
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-11-22 15:15:29 +0100

    old school nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+let
+  nixpkgs = import (import nix/sources.nix { }).nixpkgs { };
+  # hp = nixpkgs.pkgsStatic.haskell.compiler.ghc924 { };
+  # hp = nixpkgs.pkgsStatic.haskellPackages.ghc9_2_4;
+  hp = nixpkgs.pkgsStatic.haskell.packages.ghc925;
+  #   overrides = self: super: {
+  #     vty = (super.vty.override {
+  #       terminfo = super.terminfo_0_4_1_5;
+  #     });
+  #   };
+  # };
+in
+hp.callCabal2nix "random-ising-3d" ./. { }
