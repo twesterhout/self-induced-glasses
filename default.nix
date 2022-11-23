3d44788c2812a53e2164bc682885da32f4eb766d
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-11-23 13:51:28 +0100

    Switch to haskell-updates branch to benefit from caching

--- default.nix
+++ default.nix
@@ -2,7 +2,8 @@ let
   nixpkgs = import (import nix/sources.nix { }).nixpkgs { };
   # hp = nixpkgs.pkgsStatic.haskell.compiler.ghc924 { };
   # hp = nixpkgs.pkgsStatic.haskellPackages.ghc9_2_4;
-  hp = nixpkgs.haskell.packages.ghc924;
+  # hp = nixpkgs.pkgsStatic.haskell.packages.ghc924;
+  hp = nixpkgs.pkgsStatic.haskell.packages.native-bignum.ghc924;
   #   overrides = self: super: {
   #     vty = (super.vty.override {
   #       terminfo = super.terminfo_0_4_1_5;
